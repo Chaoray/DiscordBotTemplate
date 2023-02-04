63fab3bb60fc75b0e3f95a74a5d1dda6747e2a7c
Hino Shimizu <65358638+Chaoray@users.noreply.github.com>
2023-02-04 18:57:32 +0800

    ready execute once

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 const { Events, } = require('discord.js');
 
 module.exports = {
+    once: true,
     name: Events.ClientReady,
     async execute(client) {
         console.info('[CLIENT] ready');
